901b57df8cb0bf3c86f19b9cca73b682cdaff57b
stepheeeen <stephenon56@gmail.com>
2024-06-25 08:57:03 +0100

    progress

--- app/Component/Slider/Slider.jsx
+++ app/Component/Slider/Slider.jsx
@@ -1,94 +1,22 @@
-import { useState } from 'react';
-import Image from 'next/image';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './Slider.module.css'
 
-const images = [
-  '/images/img1.jpg',
-  '/images/img2.jpg',
-  '/images/img3.jpg',
-  '/images/img4.jpg',
-  '/images/img5.jpg',
-];
-
-const ImageSlider = () => {
-  const [current, setCurrent] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
-  const [modalImage, setModalImage] = useState('');
-
-  const nextSlide = () => {
-    setCurrent(current === images.length - 1 ? 0 : current + 1);
-  };
-
-  const prevSlide = () => {
-    setCurrent(current === 0 ? images.length - 1 : current - 1);
-  };
-
-  const openModal = (image) => {
-    setModalImage(image);
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
+const Slider = () => {
   return (
-    <div className="relative">
-      <div className="flex overflow-hidden">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`w-full flex-none transition-transform duration-500 transform ${
-              current === index ? 'translate-x-0' : 'translate-x-full'
-            }`}
-          >
-            <Image
-              src={image}
-              alt={`Image ${index + 1}`}
-              width={500}
-              height={300}
-              onClick={() => openModal(image)}
-              className="cursor-pointer"
-            />
-          </div>
-        ))}
-      </div>
-      <button
-        className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-white text-black p-2"
-        onClick={prevSlide}
-      >
-        Prev
-      </button>
-      <button
-        className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-white text-black p-2"
-        onClick={nextSlide}
-      >
-        Next
-      </button>
-
-      <div className="flex justify-center mt-2">
-        {images.map((_, index) => (
-          <div
-            key={index}
-            className={`h-2 w-2 rounded-full mx-1 cursor-pointer ${
-              current === index ? 'bg-black' : 'bg-gray-400'
-            }`}
-            onClick={() => setCurrent(index)}
-          />
-        ))}
-      </div>
-
-      {isOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-white p-4">
-            <Image src={modalImage} alt="Modal Image" width={500} height={300} />
-            <button className="mt-2 p-2 bg-red-500 text-white" onClick={closeModal}>
-              Close
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
+    <Swiper
+      slidesPerView={3}  // Number of slides to show
+      navigation        // Enable navigation
+      pagination={{ clickable: true }} // Enable pagination
+    >
+      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
+      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
+      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
+      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
+      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
+      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
+    </Swiper>
   );
 };
 
-export default ImageSlider;
+export default Slider;
