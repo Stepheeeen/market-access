f1f8fbdfb85098b3505329870b37de82693db68b
stepheeeen <stephenon56@gmail.com>
2024-06-25 17:26:18 +0100

    handleClose

--- app/Component/Slider/Slider.jsx
+++ app/Component/Slider/Slider.jsx
@@ -1,21 +1,55 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import './Slider.module.css'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const Slider = () => {
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <Swiper
-      slidesPerView={3}  // Number of slides to show
-      navigation        // Enable navigation
-      pagination={{ clickable: true }} // Enable pagination
-    >
-      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
-      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
-      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
-      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
-      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
-      <SwiperSlide className='p-[30px]'><div style={{background: 'red', height: '100px'}}>Slide 1</div></SwiperSlide>
-    </Swiper>
+    <div className='w-ful'>
+      <Swiper
+        slidesPerView={3}  // Number of slides to show
+        navigation        // Enable navigation
+        pagination={{ clickable: true }} // Enable pagination
+      >
+        <SwiperSlide className='p-[30px]'><div className='bg-white h-[200px] cursor-pointer' onClick={handleOpen}>Slide 1</div></SwiperSlide>
+      </Swiper>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+
+    </div>
   );
 };
 
